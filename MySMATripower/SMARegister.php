a6f883d1f147c8c5bee76ec8f72caf3426457ea8
Ralf1910 <home@ralf-ehren.de>
2023-01-06 19:36:15 +0100

    Update SMARegister.php

--- MySMATripower/SMARegister.php
+++ MySMATripower/SMARegister.php
@@ -576,14 +576,14 @@ class SMARegister
 		32201 => [
 		'name' => 'CmpBMS GetBatChaWh',			// Batterieladung der Batterie
                 'count' => 4,
-                'type' => 'RAW',
+                'type' => 'U64',
                 'format' => 'FIX0',
                 'profile' => '~Electricity'
             ],					   	
 	    32205 => [
 		'name' => 'CmpBMS GetBatDschWh',			// Batterieladung der Batterie
                 'count' => 4,
-                'type' => 'RAW',
+                'type' => 'U64',
                 'format' => 'FIX0',
                 'profile' => '~Electricity'
             ]
